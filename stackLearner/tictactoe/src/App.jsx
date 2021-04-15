e6f21c44fa0cb6f44e35c88391f16eb573380aae
jafoor <abujafor.cse11@gmail.com>
2021-04-15 10:49:08 +0600

    Tic Tac Toe

---
+++ stackLearner/tictactoe/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Game from './component/game'
+const App = () => {
+  return (
+    <div className='app'>
+      <h1>Tic Tac Toe</h1>
+      <Game
+      />
+    </div>
+  )
+}
+
+export default App;
