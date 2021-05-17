706fcbb039d0cd64a0d59c0d2a900b3329e7ea37
jafoor <abujafor.cse11@gmail.com>
2021-05-17 21:16:50 +0600

    Update

--- Bohubrihi/hello-world/src/App.js
+++ Bohubrihi/hello-world/src/App.js
@@ -12,11 +12,21 @@ class App extends Component {
     otherProp: "I am some other Prop"
   }
 
-  changeBookState = () => {
+  changeBookState = newBookName => {
     // Wrong: this.state.books[0].bookName = "1974" 
     this.setState({
       books: [
-        { bookName: "Nineteen Eighty-Four", writer: "George Orwell" },
+        { bookName: newBookName, writer: "George Orwell" },
+        { bookName: "The Da Vinci Code", writer: "Dan Brown" },
+        { bookName: "Metmorphosis", writer: "Franz Kafka" }
+      ]
+    });
+  }
+
+  changeWithInputState = event => {
+    this.setState({
+      books: [
+        { bookName: event.target.value, writer: "George Orwell" },
         { bookName: "The Da Vinci Code", writer: "Dan Brown" },
         { bookName: "Metmorphosis", writer: "Franz Kafka" }
       ]
@@ -24,26 +34,37 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      border: "1px solid red",
+      borderRadius: "5px",
+      backgroundColor: "black",
+      color: "white",
+    };
     console.log(this.state);
     return (
       <div className="App">
-        <h1>Book List</h1>
-        <button onClick={this.changeBookState}>Change State</button>
+        <h1 style={style}>Book List</h1>
+        <button onClick={() => this.changeBookState("Nineteen Eighty-Four")}>Change State</button>
+        <input type="text" onChange={this.changeWithInputState} />
         <Book
           bookName={this.state.books[0].bookName}
-          writer={this.state.books[0].writer} />
+          writer={this.state.books[0].writer}
+          inputName={this.changeWithInputState} />
         <Book
           bookName={this.state.books[1].bookName}
           writer={this.state.books[1].writer} />
         <Book
           bookName={this.state.books[2].bookName}
-          writer={this.state.books[2].writer} />
+          writer={this.state.books[2].writer}
+          change={this.changeBookState.bind(this, "Nineteen 84")} />
       </div>
     );
   }
 }
 
 export default App;
+
+
 
 
 // cmd + k + c for comment
