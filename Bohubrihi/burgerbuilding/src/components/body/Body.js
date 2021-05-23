9508daa7048c3c3640b09150ced9da856a8a7e06
jafoor <abujafor.cse11@gmail.com>
2021-05-23 08:31:13 +0600

    Update Burger Project

--- Bohubrihi/burgerbuilding/src/components/body/Body.js
+++ Bohubrihi/burgerbuilding/src/components/body/Body.js
@@ -3,16 +3,19 @@ import Menu from './Menu';
 import Home from './Home';
 import Contact from './Contact';
 import About from './About';
-import {Route, Redirect} from 'react-router-dom';
+import {Route, Redirect, Switch} from 'react-router-dom';
 
 const Body = () => {
     return (
         <div>
+       
            <Route path="/" exect component={Home}/>
            <Route path="/menu" exect component={Menu}/>
            <Route path="/about" exect component={About}/>
            <Route path="/Contact" exect component={Contact}/>
            <Redirect from="/" to ="/home"/>
+
+       
         </div>
     )
 }
